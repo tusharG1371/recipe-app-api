c2f35ff65f3c8abe8d80b9f250056f5f1b8a66ba
Tushar Agalabs Lenovo <tushar@agalabs.com>
2023-04-01 01:55:06 +0530

    not sure where

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,12 @@
 """
-Calculator Function
+Calculator function
 """
+ 
  
 def add(x, y):
     """Add x and y and return result."""
     return x + y
+ 
  
 def subtract(x, y):
     """Subtract x from y and return result."""
--- app/core/tests/tests_commands.py
+++ app/core/tests/tests_commands.py
@@ -8,20 +8,21 @@ from django.db.utils import OperationalError
 from django.test import TestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class CommandsTest(TestCase):
-    """Test Commands."""
+class CommandTests(TestCase):
+    """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
-        """Test waiting for database when database is ready"""
+        """Test waiting for database if database is ready."""
         patched_check.return_value = True
         call_command('wait_for_db')
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
-        patched_check.side_effect = (Psycopg2Error)*2 + [OperationalError] * 3 + [True]
+        patched_check.side_effect = (Psycopg2Error)*2 + \
+            [OperationalError] * 3 + [True]
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
 
