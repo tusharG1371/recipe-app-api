68740638b969b4ef23dbc50b2c3c774416c386b2
Tushar Agalabs Lenovo <tushar@agalabs.com>
2023-04-01 02:05:09 +0530

    t

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,5 +1,5 @@
 """
-Calculator function
+Calculator functions
 """
  
  
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,13 +2,13 @@
 Test custom Django management command 
 """
 from unittest.mock import patch
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class CommandTests(TestCase):
+class CommandTests(SimpleTestCase):
     """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
@@ -20,7 +20,7 @@ class CommandTests(TestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
-        patched_check.side_effect = (Psycopg2Error)*2 + \
+        patched_check.side_effect = [Psycopg2OpError]*2 + \
             [OperationalError] * 3 + [True]
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
