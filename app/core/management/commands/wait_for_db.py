cce6159762b5dcd6a53152961ded501d55c08cd2
Tushar Agalabs Lenovo <tushar@agalabs.com>
2023-04-01 13:53:52 +0530

    t

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -18,8 +18,8 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2OpError, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 second ...')
+            except (Psycopg2OpError, OperationalError) as e:
+                self.stdout.write('Database unavailable, \
+                                   waiting second :{}'.format(e))
                 time.sleep(1)
-    
         self.stdout.write(self.style.SUCCESS('Database available!'))
