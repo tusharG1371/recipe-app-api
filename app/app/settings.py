4f17ac8bf68fb2a8723915770751d97d2fb6a804
Tushar Agalabs Lenovo <tushar@agalabs.com>
2023-04-17 16:38:06 +0530

    tushar

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 STATIC_URL = '/static/static/'
-MEDIA_URL = '/static/media'
+MEDIA_URL = '/static/media/'
 
 MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
@@ -144,4 +144,9 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema', 
+}
+
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
+    
 }
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ Tests for models.
 from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from unittest.mock import patch
 
 from core import models
 
@@ -83,6 +84,14 @@ class ModelTests(TestCase):
             name='Ingredient'
         )
         self.assertEqual(str(ingredient), 'Ingredient')
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path."""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
 
     
     
